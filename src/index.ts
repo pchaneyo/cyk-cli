96a2f5c905fd3472c30d5376807e04869a2dc91d
philippe chaneyoukaye <philippe.cyk@gmail.com>
2023-12-14 22:10:26 +0400

    0.11.3 depends on @cyklang/core@0.8.3

--- src/index.ts
+++ src/index.ts
@@ -176,7 +176,7 @@ class StopCommand extends Command {
  */
 const program = new Command()
 program.name('cyk').description('cyklang CLI')
-    .version('0.11.2')
+    .version('0.11.3')
 program.addCommand(new StartCommand('start'))
 program.addCommand(new StopCommand('stop'))
 program.addCommand(new InstallCommand('install'))
